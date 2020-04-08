e2c27b84e28402f49d054e84c253c122d0c6a56f
teenietinyjess <60458212+teenietinyjess@users.noreply.github.com>
2020-04-08 16:19:40 -0700

    Add files via upload

--- Sensor.java
+++ Sensor.java
@@ -1,4 +1,18 @@
+import java.util.Random;
 
 public class Sensor {
+	
+	public static void main(String[] args) {
+        // Numbers are for temperature
+		int theMin = -40;
+		int theMax = 150;
 
+		System.out.println("Random Temperature: " + generateData(theMin, theMax));
+	}
+    
+    // Just makes data for temperature right now
+	public static int generateData(int myMin, int myMax) {
+		Random random = new Random();
+		return random.nextInt((myMin - myMax) + 1) + myMin;
+	}
 }
