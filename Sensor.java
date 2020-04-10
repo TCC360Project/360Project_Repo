81e627d6883941c6ec20c3f4ff6c406cc82b1ef0
JacobSousie <63326926+JacobSousie@users.noreply.github.com>
2020-04-10 13:59:01 -0700

    Add files via upload
    I added a method round(value, places) to take the data from the sensors and round them two decimal places.  These values are exact and still in double format if they needed to be used anywhere else.

--- Sensor.java
+++ Sensor.java
@@ -1,51 +1,56 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.Random;
 
 public class Sensor {
-    private final double theTemperature;
-    private final double theHumidity;
-    private final double theWind;
-    private final double theRain;
+	private final double theTemperature;
+	private final double theHumidity;
+	private final double theWind;
+	private final double theRain;
 
-    /**
-     * Data chosen based off weather at Jess's house.
-     * To be used as initial data point.
-     */
-    public Sensor() {
-        theTemperature = 60;
-        theHumidity = 44;
-        theWind = 10;
-        theRain = 0;
-        
-    }
-    
-    /**
-     * Meant to provide a thread of data that makes sense together.
-     * @param currentData the most recent sensor
-     */
-    public Sensor(Sensor currentData) {
-    	// Assign new sensor readings based off of currentData's.
-    	theTemperature = generateNotRainData(currentData.getTheTemperature());
-    	theHumidity = generateNotRainData(currentData.getTheHumidity());
-    	theWind = generateNotRainData(currentData.getTheWind());
-    	theRain = generateRainData(currentData.getTheRain());
-    	
-    }
-    
-    /**
-     * Resets theRain amount for new day.
-     * Sets theTemperature,theHumidity, and theWind based off most recent sensor.
-     * @param myTemperature theTemperature from most recent sensor.
-     * @param myHumidity theHumidity from most recent sensor.
-     * @param myWind theWind from most recent sensor.
-     */
-    public Sensor(double myTemperature, double myHumidity, double myWind) {
-    	// Assign new sensor readings based off parameters
-    	theTemperature = generateNotRainData(myTemperature);
-    	theHumidity = generateNotRainData(myHumidity);
-    	theWind = generateNotRainData(myWind);
-    	// Reset theRain to 0 for new day
-    	theRain = 0;
-    }
+	/**
+	 * Data chosen based off weather at Jess's house. To be used as initial data
+	 * point.
+	 */
+	public Sensor() {
+		theTemperature = 60;
+		theHumidity = 44;
+		theWind = 10;
+		theRain = 0;
+
+	}
+
+	/**
+	 * Meant to provide a thread of data that makes sense together.
+	 * 
+	 * @param currentData the most recent sensor
+	 */
+	public Sensor(Sensor currentData) {
+		// Assign new sensor readings based off of currentData's.
+		theTemperature = generateNotRainData(currentData.getTheTemperature());
+		theHumidity = generateNotRainData(currentData.getTheHumidity());
+		theWind = generateNotRainData(currentData.getTheWind());
+		theRain = generateRainData(currentData.getTheRain());
+
+	}
+
+	/**
+	 * Resets theRain amount for new day. Sets theTemperature,theHumidity, and
+	 * theWind based off most recent sensor.
+	 * 
+	 * @param myTemperature theTemperature from most recent sensor.
+	 * @param myHumidity    theHumidity from most recent sensor.
+	 * @param myWind        theWind from most recent sensor.
+	 */
+	public Sensor(double myTemperature, double myHumidity, double myWind) {
+		// Assign new sensor readings based off parameters
+		theTemperature = generateNotRainData(myTemperature);
+		theHumidity = generateNotRainData(myHumidity);
+		theWind = generateNotRainData(myWind);
+		// Reset theRain to 0 for new day
+		theRain = 0;
+	}
 
 	public double getTheTemperature() {
 		return theTemperature;
@@ -62,75 +67,93 @@ public class Sensor {
 	public double getTheRain() {
 		return theRain;
 	}
-	
+
 	public String toString() {
-		return "Temperature: " + this.getTheTemperature() 
-		+ " Humidity: " + this.getTheHumidity() + " Wind: " + this.getTheWind() 
-		+ " Rain: " + this.getTheRain();
-		
+		return "Temperature: " + this.getTheTemperature() + " Humidity: " + this.getTheHumidity() + " Wind: "
+				+ this.getTheWind() + " Rain: " + this.getTheRain();
+
 	}
-	
+
 	/*
-	 * Generates data for all sensors besides rain.
-	 * Rain cannot decrease in a day.
+	 * Generates data for all sensors besides rain. Rain cannot decrease in a day.
 	 */
 	public double generateNotRainData(double theData) {
 		Random random = new Random();
-		
+
 		// Determines if data will increase or decrease
-    	boolean boolDirection = random.nextBoolean();
-    	int direction;
-    	if (boolDirection) {
-    		direction = 1;
-    	} else {
-    		direction = -1;
-    	}
-    	
-    	// Determines magnitude of data change
-    	// Allows no more larger change than 1%
-    	int change = random.nextInt(11);
-    	
-		if (theData != 0.0) {
-			return (theData * (direction * change/1000.0)) + theData;
+		boolean boolDirection = random.nextBoolean();
+		int direction;
+		if (boolDirection) {
+			direction = 1;
 		} else {
-			return (direction * change/1000.0) + theData;
+			direction = -1;
+		}
+
+		// Determines magnitude of data change
+		// Allows no more larger change than 1%
+		int change = random.nextInt(11);
+
+		if (theData != 0.0) {
+			return round((theData * (direction * change / 1000.0) + theData), 2);
+		} else {
+			return round((direction * change / 1000.0 + theData), 2);
 		}
 	}
-	
+
 	public double generateRainData(double theData) {
 		Random random = new Random();
-		
+
 		// Determines if rain will increase (T) or stay the same (F)
 		boolean willChange = random.nextBoolean();
-    	if (!willChange) {
-    		return theData;
-    	} else {
-    		// Allows increase of no more than 0.01
-    		return (random.nextInt(11)/1000.0) + theData;
-    	}
+		if (!willChange) {
+			return round(theData, 2);
+		} else {
+			// Allows increase of no more than 0.01
+			return round((random.nextInt(11) / 1000.0 + theData), 2);
+		}
 	}
-	
+
+	static void run() {
+
+	}
+
 	/**
 	 * For testing purposes only.
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-        Sensor s1 = new Sensor();
-        Sensor s2 = new Sensor(s1);
-        Sensor s3 = new Sensor(s2);
-        Sensor s4 = new Sensor(s3);
-        Sensor s5 = new Sensor(s4);
-        Sensor s6 = new Sensor(s5);
-        Sensor s7 = new Sensor(s5.getTheTemperature(), s5.getTheHumidity(), s5.getTheWind());
-        
-        System.out.println("Sensor 1 Data: " + s1.toString());
-        System.out.println("Sensor 2 Data: " + s2.toString());
-        System.out.println("Sensor 3 Data: " + s3.toString());
-        System.out.println("Sensor 4 Data: " + s4.toString());
-        System.out.println("Sensor 5 Data: " + s5.toString());
-        System.out.println("Sensor 6 Data: " + s6.toString());
-        System.out.println("It's a new day!");
-        System.out.println("Sensor 7 Data: " + s7.toString());
-        
+		Sensor s1 = new Sensor();
+		Sensor s2 = new Sensor(s1);
+		Sensor s3 = new Sensor(s2);
+		Sensor s4 = new Sensor(s3);
+		Sensor s5 = new Sensor(s4);
+		Sensor s6 = new Sensor(s5);
+		Sensor s7 = new Sensor(s5.getTheTemperature(), s5.getTheHumidity(), s5.getTheWind());
+
+		System.out.println("Sensor 1 Data: " + s1.toString());
+		System.out.println("Sensor 2 Data: " + s2.toString());
+		System.out.println("Sensor 3 Data: " + s3.toString());
+		System.out.println("Sensor 4 Data: " + s4.toString());
+		System.out.println("Sensor 5 Data: " + s5.toString());
+		System.out.println("Sensor 6 Data: " + s6.toString());
+		System.out.println("It's a new day!");
+		System.out.println("Sensor 7 Data: " + s7.toString());
+
+	}
+
+	/**
+	 * This rounding method will take whatever value is passed and round it to a
+	 * specified place value
+	 * 
+	 * @param value  The number that will be rounded
+	 * @param places The number of place values that you will round to
+	 */
+	public static double round(double value, int places) {
+		if (places < 0)
+			throw new IllegalArgumentException();
+		BigDecimal bigDecimalValue = BigDecimal.valueOf(value);
+		bigDecimalValue = bigDecimalValue.setScale(places, RoundingMode.HALF_UP);
+		return bigDecimalValue.doubleValue();
 	}
 }
